7a6fa57377bfaa47a8341f2919e708a55d4e10e5
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-12-10 13:28:43 +0300

    detect buttons implemented

--- 7 Swifty Words/ViewController.swift
+++ 7 Swifty Words/ViewController.swift
@@ -14,10 +14,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var answersLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var currentAnswer: UITextField!
+    @IBOutlet weak var buttonsStack: UIStackView!
+    
+    var letterButtons = [UIButton]()
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
+    var score = 0
+    var level = 1
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for stackView in buttonsStack.subviews {
+            for subview in stackView.subviews where subview.tag == 1001 {
+                let btn = subview as! UIButton
+                letterButtons.append(btn)
+                btn.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+            }
+        }
+        
+    }
+    
+    @objc func letterTapped(){
+        
     }
     
     //MARK: -IBActions
